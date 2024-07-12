8cdb1112fe43f94269cb3375465c9f9074afc1e2
jmlee <lee00litt@gmail.com>
2024-07-12 11:56:36 +0900

    -비밀번호 설정 화면

--- app/src/main/java/com/example/ourmenu/SignupPwFragment.kt
+++ app/src/main/java/com/example/ourmenu/SignupPwFragment.kt
@@ -5,23 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.ourmenu.databinding.FragmentSignupEmailSertifyBinding
+import com.example.ourmenu.databinding.FragmentSignupPwBinding
 
 
-class SignupEmailSertifyFragment : Fragment() {
-    lateinit var binding:FragmentSignupEmailSertifyBinding
-
+class SignupPwFragment : Fragment() {
+    lateinit var binding : FragmentSignupPwBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = FragmentSignupEmailSertifyBinding.inflate(layoutInflater)
-
+        binding = FragmentSignupPwBinding.inflate(layoutInflater)
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        // Inflate the layout for this fragment
         return binding.root
     }
+
 
 }
