54b87c48a2d18eca0b7c4bfb6e60d6897a2966ca
jmlee <lee00litt@gmail.com>
2024-07-12 20:21:46 +0900

    design : 비밀번호 보이기 체크박스 제작

---
+++ app/src/main/java/com/example/ourmenu/SignupNameFragment.kt
@@ -0,0 +1,26 @@
+package com.example.ourmenu
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.ourmenu.databinding.FragmentSignupNameBinding
+
+class SignupNameFragment : Fragment() {
+    lateinit var binding : FragmentSignupNameBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = FragmentSignupNameBinding.inflate(layoutInflater)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return binding.root
+    }
+
+
+}
