1f1a33f2a933a8e183e27ff86e3a4c87dc9b7005
Naeun Kim <102296721+Nico1eKim@users.noreply.github.com>
2024-08-17 02:56:50 +0900

    chore: sample 이미지 모두 없애기

--- app/src/main/java/com/example/ourmenu/home/adapter/HomeMenuMainRVAdapter.kt
+++ app/src/main/java/com/example/ourmenu/home/adapter/HomeMenuMainRVAdapter.kt
@@ -3,28 +3,28 @@ package com.example.ourmenu.home.adapter
 import android.content.Context
 import android.util.Log
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.bumptech.glide.Glide
 import com.example.ourmenu.R
-import com.example.ourmenu.data.HomeMenuData
 import com.example.ourmenu.data.onboarding.data.OnboardingMenuData
 import com.example.ourmenu.databinding.ItemHomeMenuMainBinding
 import com.example.ourmenu.home.iteminterface.HomeItemClickListener
 
-class HomeMenuMainRVAdapter(val items: ArrayList<OnboardingMenuData>, val context: Context) :
-    RecyclerView.Adapter<HomeMenuMainRVAdapter.ViewHolder>() {
-
+class HomeMenuMainRVAdapter(
+    val items: ArrayList<OnboardingMenuData>,
+    val context: Context,
+) : RecyclerView.Adapter<HomeMenuMainRVAdapter.ViewHolder>() {
     private lateinit var itemClickListener: HomeItemClickListener
 
     fun setOnItemClickListener(onItemClickListener: HomeItemClickListener) {
         itemClickListener = onItemClickListener
     }
 
-    inner class ViewHolder(private val binding: ItemHomeMenuMainBinding) : RecyclerView.ViewHolder(binding.root) {
-
+    inner class ViewHolder(
+        private val binding: ItemHomeMenuMainBinding,
+    ) : RecyclerView.ViewHolder(binding.root) {
         fun bind(item: OnboardingMenuData) {
             // 아이템 클릭 리스너 추가
             binding.root.setOnClickListener {
@@ -36,28 +36,33 @@ class HomeMenuMainRVAdapter(val items: ArrayList<OnboardingMenuData>, val contex
 //
             if (item.menuImgUrl != "null" && item.menuImgUrl != "" && item.menuImgUrl != null) {
                 Log.d("glide", item.menuImgUrl)
-                Glide.with(context)
+                Glide
+                    .with(context)
                     .load(item.menuImgUrl)
                     .into(binding.sivItemMenuImageMain)
                 binding.sivItemMenuImageMain.layoutParams.width =
                     (context.resources.displayMetrics.widthPixels * 304 / 360).toInt()
             } else {
-                binding.sivItemMenuImageMain.setImageResource(R.drawable.menu_sample)
+                binding.sivItemMenuImageMain.setImageResource(R.drawable.default_image)
             }
         }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int,
+    ): ViewHolder {
         val binding = ItemHomeMenuMainBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return ViewHolder(binding)
     }
 
     override fun getItemCount(): Int = 2000
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    override fun onBindViewHolder(
+        holder: ViewHolder,
+        position: Int,
+    ) {
         if (items.size == 0) return
         holder.bind(items[position % items.size])
     }
-
-
 }
