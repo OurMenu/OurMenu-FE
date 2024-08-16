eae61c774a50585a37141fa151ea62f6634961cc
Naeun Kim <102296721+Nico1eKim@users.noreply.github.com>
2024-08-16 16:59:31 +0900

    refactor: 회원가입 이메일 hint값 수정

--- app/src/main/java/com/example/ourmenu/account/adapters/SpinnerAdapter.kt
+++ app/src/main/java/com/example/ourmenu/account/adapters/SpinnerAdapter.kt
@@ -8,17 +8,21 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.TextView
-import androidx.core.widget.addTextChangedListener
 import com.example.ourmenu.R
 import com.example.ourmenu.account.SignupEmailFragment
-import com.example.ourmenu.databinding.FragmentSignupEmailBinding
 import com.example.ourmenu.databinding.SpinnerItemBackgroundBinding
 import com.example.ourmenu.databinding.SpinnerItemBackgroundEditBinding
 import com.example.ourmenu.databinding.SpinnerItemBackgroundNullBinding
 
-class SpinnerAdapter<T>(context: Context, val fragment: SignupEmailFragment) :
-    ArrayAdapter<String>(context, R.layout.spinner_default_background, listOf("텍스트", "직접 입력하기", "2", "3", "4")) {
-    override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup): View {
+class SpinnerAdapter<T>(
+    context: Context,
+    val fragment: SignupEmailFragment,
+) : ArrayAdapter<String>(context, R.layout.spinner_default_background, listOf("텍스트", "직접 입력하기", "2", "3", "4")) {
+    override fun getDropDownView(
+        position: Int,
+        convertView: View?,
+        parent: ViewGroup,
+    ): View {
         val binding = SpinnerItemBackgroundBinding.inflate(LayoutInflater.from(context), parent, false)
         val nullbinding = SpinnerItemBackgroundNullBinding.inflate(LayoutInflater.from(context), parent, false)
         if (position == 0) {
@@ -28,39 +32,53 @@ class SpinnerAdapter<T>(context: Context, val fragment: SignupEmailFragment) :
         return binding.root
     }
 
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+    override fun getView(
+        position: Int,
+        convertView: View?,
+        parent: ViewGroup,
+    ): View {
         val view = super.getView(position, convertView, parent)
         val editbinding = SpinnerItemBackgroundEditBinding.inflate(LayoutInflater.from(context), parent, false)
         if (position == 0) {
-
             view.requireViewById<TextView>(R.id.tv_spinner_default_background).apply {
                 text = ""
-                hint = "텍스트"
+                hint = "email.com"
             }
         } else if (position == 1) {
-            editbinding.tvSpinnerItemBackgroundEdit.addTextChangedListener(object : TextWatcher {
-                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-                    fragment.adflag = false
-                }
-
-                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                }
-
-                override fun afterTextChanged(s: Editable?) {
-                    if (!s.isNullOrEmpty()) {
-                        fragment.adflag = true
-                        fragment.flagCheck()
-                    }else{
+            editbinding.tvSpinnerItemBackgroundEdit.addTextChangedListener(
+                object : TextWatcher {
+                    override fun beforeTextChanged(
+                        s: CharSequence?,
+                        start: Int,
+                        count: Int,
+                        after: Int,
+                    ) {
                         fragment.adflag = false
-                        fragment.flagCheck()
                     }
-                }
-            })
+
+                    override fun onTextChanged(
+                        s: CharSequence?,
+                        start: Int,
+                        before: Int,
+                        count: Int,
+                    ) {
+                    }
+
+                    override fun afterTextChanged(s: Editable?) {
+                        if (!s.isNullOrEmpty()) {
+                            fragment.adflag = true
+                            fragment.flagCheck()
+                        } else {
+                            fragment.adflag = false
+                            fragment.flagCheck()
+                        }
+                    }
+                },
+            )
             return editbinding.root
         }
         return view
     }
-
 
     fun initListener() {
         val binding = SpinnerItemBackgroundEditBinding.inflate(LayoutInflater.from(context))
